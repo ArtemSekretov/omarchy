ec110bb3cc33f3f109971b23bb22bf56f4c2d044
Artem Sekretov <artemsekretov@gmail.com>
2025-09-03 16:04:43 +0300

    fixed teams icon

--- install/packaging/my-webapps.sh
+++ install/packaging/my-webapps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-omarchy-webapp-install "Teams" https://teams.microsoft.com/v2/ https://www.veryicon.com/download/png/business/wd-light-blue/wd-applet-teams-time?s=256
+omarchy-webapp-install "Teams" https://teams.microsoft.com/v2/ https://cdn-icons-png.freepik.com/256/906/906349.png
 omarchy-webapp-install "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
